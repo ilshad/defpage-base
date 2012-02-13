567036dbcfb54b50115f28e5f594cb53c5b43b7f
Ilshad Khabibullin <astoon@spacta.com>
2012-02-13 22:25:17 +0400

    "public" is commented in

--- defpage/base/__init__.py
+++ defpage/base/__init__.py
@@ -90,8 +90,8 @@ def main(global_config, **settings):
                     renderer="defpage.base:templates/transmission/overview.pt")
 
     # public collections
-    config.add_view("defpage.base.views.public_overview",
-                    "public",
-                    renderer="defpage.base:templates/public/overview.pt")
+    #config.add_view("defpage.base.views.public_overview",
+    #                "public",
+    #                renderer="defpage.base:templates/public/overview.pt")
 
     return config.make_wsgi_app()
