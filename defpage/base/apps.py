fe2e524ddee65cbad78868219355f23db196d88f
Ilshad Khabibullin <astoon@spacta.com>
2012-03-17 17:56:56 +0400

    show more information about source

--- defpage/base/apps.py
+++ defpage/base/apps.py
@@ -2,6 +2,7 @@ def get_source_types():
     return [
         {"id":"gd",
          "title":"Google Docs",
-         "url":"http://localhost:9020"}
+         "url":"http://localhost:9020",
+         "special_attr":"folder_id"}
         ]
     
