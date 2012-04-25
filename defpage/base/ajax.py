1f94fccd6a5e5bccc0f8d50c605bea7559ee3b01
Ilshad Khabibullin <astoon@spacta.com>
2012-04-25 19:13:18 +0400

    used ajax load for documents listing

--- defpage/base/ajax.py
+++ defpage/base/ajax.py
@@ -2,7 +2,7 @@ from defpage.base import meta
 
 def get_collection_documents(req):
     docs = []
-    for i in meta.get_collection_documents(req.user.usrid, req.matchdict["name"]):
+    for i in meta.get_collection_documents(req.user.userid, req.matchdict["name"]):
         docs.append({"id":           i["id"],
                      "title":        i["title"],
                      "source_css":   i["source"]["type"] == "gd" and "source_css_gd"})
